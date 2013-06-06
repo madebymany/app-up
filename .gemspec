c39cce872c3737d149574fbd2a4667ef22add809
Alex Barlow <alexbarlowis@gmail.com>
2013-06-06 16:38:58 +0100

    Initial version

---
+++ .gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name        = 'app=up'
+  s.version     = '0.0.1'
+  s.summary     = "A gem for distributing Andriod and iOS apps"
+  s.description = "A gem for distributing Andriod and iOS apps and generating links in ruby"
+  s.authors     = ["Alex Barlow"]
+  s.email       = 'alexb@madebymany.co.uk'
+  s.files       = ["lib/app-up.rb", "lib/image_57x57.png", "lib/image_512x512.png"]
+  s.executables << 'app-up'
+  s.add_runtime_dependency "thor"
+  s.add_runtime_dependency "fog"
+  s.add_runtime_dependency "activesupport"
+end
---
+++ lib/app-up.rb
@@ -0,0 +1,123 @@
+require 'singleton'
+require 'pathname'
+require 'fog'
+require "erb"
+require 'active_support/inflector'
+
+class AppUp
+
+  include Singleton
+  include ActiveSupport::Inflector
+
+  attr_accessor :settings, :fog_connection, :bucket, :urls
+
+  DEFAULT_YAML_SETTINGS = {
+    s3: {
+      access_key_id: 'replaceme',
+      secret_access_key: 'replaceme',
+      region: 'eu-west-1',
+      bucket: 'apps'
+    },
+    app_name: 'YourAppName',
+    version: '0.0.1',
+    bundle_identifier: 'com.example.your-app-name',
+    ios: {
+      available: true
+    },
+    andriod: {
+      available: true
+    },
+    icon: {
+      regular: 'image_57x57.png',
+      retina:  'image_512x512.png'
+    }
+  }
+
+  def get_binding
+    binding
+  end
+
+  def self.load_settings
+    AppUp.instance.settings = 
+        File.open( 'app-up.yaml' ) { |yf| YAML::load( yf ) }
+  end
+
+  def self.upload_icons
+    settings = AppUp.load_settings
+    icons = [settings[:icon][:regular], settings[:icon][:retina]]
+    icons.each do |icon|
+      upload_file icon, path_with_name(icon)
+    end
+  end
+
+  def self.upload_app(app_path, version)
+    new_path = path_with_name filepath_with_version(app_path, version)
+    upload_file app_path, new_path
+  end
+
+  def self.generate_manifest
+    s3_connect
+    instance.urls = {
+      regular: instance.bucket.files.new(
+           key: path_with_name(instance.settings[:icon][:regular]))
+                .url(Time.new.to_i + 60*60*24),
+      retina: instance.bucket.files.new(
+           key: path_with_name(instance.settings[:icon][:retina]))
+                .url(Time.new.to_i + 60*60*24),
+      app: instance.bucket.files.new(
+           key: path_with_name(
+                  filepath_with_version(nil, instance.settings[:version]))
+                ).url(Time.new.to_i + 60*60*24),
+    }
+
+    template = File.read(File.dirname(__FILE__) + "/manifest.plist.erb")
+    ERB.new(template).result(instance.get_binding)
+  end
+
+  def self.s3_connect
+    settings = load_settings
+
+    instance.fog_connection ||= Fog::Storage.new({
+      :provider                 => 'AWS',
+      :aws_access_key_id        => settings[:s3][:access_key_id],
+      :aws_secret_access_key    => settings[:s3][:secret_access_key],
+      :region                   => settings[:s3][:region]
+    })
+
+    AppUp.instance.bucket ||=
+      instance.fog_connection.directories.get(settings[:s3][:bucket])
+  end
+
+  def self.upload_file(path, new_filename)
+    s3_connect
+    puts "Uploading #{new_filename}"
+    AppUp.instance.bucket.files.create(
+      :key    => new_filename,
+      :body   => File.open(path),
+      :public => false
+    )
+  end
+
+  def self.filepath_with_version(path = nil, version)
+    if path
+      extention = Pathname.new(path).basename.to_s.split('.').last 
+    else
+      extention = '.ipa'
+    end
+    "#{instance.settings[:app_name].parameterize}-#{version}.#{extention}"
+  end
+
+  def self.paramed_app_name
+    load_settings[:app_name].parameterize
+  end
+
+  def self.path_with_name(name)
+    "#{paramed_app_name}/#{name}"
+  end
+
+  def self.exit_with_error(message)
+    puts "Error: #{message}"
+    exit
+  end
+
+end
